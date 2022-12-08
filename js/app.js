0c078b284c1c258b151e4702b31aaf3769204995
anisur134 <anisurrahmang134@gmail.com>
2022-12-08 21:20:57 +0600

    education website first commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll',()=>{
+
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
+})
+
+
+var faqs=document.querySelectorAll('.faq')
+
+faqs.forEach(faq =>{
+faq.addEventListener('click',()=>{
+
+    faq.classList.toggle('open');
+
+    const icon=faq.querySelector('.faq__icon i');
+if(icon.className==='uil uil-plus'){
+    icon.className='uil uil-minus';
+}else{
+    icon.className='uil uil-plus';
+}
+})
+
+})
+   const navMenu=document.querySelector(".nav__menu");
+   const openBtn =document.querySelector("#open-menu-btn");
+   const closeBtn=document.querySelector("#close-menu-btn");
+
+   openBtn.addEventListener('click',()=>{
+     
+         navMenu.style.display="flex";
+         closeBtn.style.display="inline-block";
+         openBtn.style.display="none";
+
+   })
+
+     const closeNav=()=>{
+         
+        navMenu.style.display="none";
+        closeBtn.style.display="none";
+        openBtn.style.display="inline-block";
+
+     }
+    
+     closeBtn.addEventListener('click',closeNav);
+
+
